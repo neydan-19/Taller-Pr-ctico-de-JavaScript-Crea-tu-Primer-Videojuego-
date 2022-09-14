fa4fb9e07c871dfb9cbd9708ac2e800070de1b8e
Brad <bradvelarde@gmail.com>
2022-09-14 18:03:09 -0500

    acabe mi proyecto

---
+++ maps.js
@@ -0,0 +1,61 @@
+const emojis = {
+    "-": " ",
+    O: "🚪",
+    X: "💣",
+    I: "🎁",
+    PLAYER: "💀",
+    BOMB_COLLISION: "🔥",
+    GAME_OVER: "👎",
+    WIN: "🏆",
+    HEART: "❤",  
+  };
+const maps = [];
+  maps.push(`
+    IXXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    -XXXXXXXXX
+    OXXXXXXXXX
+  `);
+  maps.push(`
+    O--XXXXXXX
+    X--XXXXXXX
+    XX----XXXX
+    X--XX-XXXX
+    X-XXX--XXX
+    X-XXXX-XXX
+    XX--XX--XX
+    XX--XXX-XX
+    XXXX---IXX
+    XXXXXXXXXX
+    `);
+  maps.push(`
+    I-----XXXX
+    XXXXX-XXXX
+    XX----XXXX
+    XX-XXXXXXX
+    XX-----XXX
+    XXXXXX-XXX
+    XX-----XXX
+    XX-XXXXXXX
+    XX-----OXX
+    XXXXXXXXXX
+  `)
+  maps.push(`
+    O--XXXXXXX
+    XX-XX---XX
+    XX----X-XX
+    XXXXXXX-XX
+    XXXX----XX
+    -----XXXXX
+    -XXXXX----
+    -------XX-
+    XXXXXXXX--
+    I--------X
+  `);
+  
